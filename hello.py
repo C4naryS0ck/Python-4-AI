b2614b3b76ce313ec9e2d77b7c5615caca628edc
Abhishree Raj <abhishree03star@gmail.com>
2026-04-24 21:30:19 +0530

    importing a library

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print("Hello, World!")
 print("I am learning AI!!")
-print("")
+import requests 
+response = requests.get("https://api.github.com")
+print(response.status_code) #should return 200 if successful.
