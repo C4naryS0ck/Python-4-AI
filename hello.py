5f57db62f647330971fef83fdc3014648da13f51
Abhishree Raj <abhishree03star@gmail.com>
2026-05-03 23:00:37 +0530

    data type, variables, type conversions, inputting

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
+'''
 print("Hello, World!")
 print("I am learning AI!!")
-'''import requests 
+import requests 
 response = requests.get("https://api.github.com")
 print(response.status_code) #should return 200 if successful.
-'''
+
 first_name = 'abhi'
 last_name = "raj"
 
@@ -31,4 +32,73 @@ print(can_drive)
 
 string = "hi my name is if"
 name = f'if {string}'
-print(name)
+print(name)  
+'''
+
+"""name = "abhishree" # string variable
+age = 33
+price = 23.444"""
+
+"""print("my name is:",name)
+print("my age is:",age)
+print("my price is:",price) 
+print(type(name))
+print(type(age))    
+z = None
+print(type(z))
+print(z)
+"""
+#print sum
+"""a = 2
+b = 3
+sum = a+b
+print(sum) 
+print(a+b)
+print(a-b)"""
+#arithem operators
+z = 5
+x = 2
+"""print(z+x)
+print(z-x)
+print(z*x)
+print(z/x)
+print(z//x) #floor division
+print(z%x) #modulus
+print(z**x) #exponentiation
+print(z==x)
+print(z!=x)
+print(z>x)
+print(z<x)
+print(z>=x)
+print(z<=x)"""
+
+#logical operators
+'''a = 50
+b = 30
+print(not False)
+print(not True) 
+print(a>b and b<a)
+print(a>b or b<a) 
+print(not(a>b and b<a))
+print(not(a>b or b<a)) '''
+
+'''#type conversion
+a,b  = 1,2.0
+print(int(a+b))
+
+h = 3.13
+h = str(h)
+j = 'u'
+print(type(h),h+j)'''
+'''
+#input function
+name = input("what is your name? ")
+print("welcome",name) 
+
+name1 = int(input("what is your name? "))
+print("welcome",name1) ''' 
+
+#Ques: WAP to input 2 numbers & print their sum.
+a = int(input('enter a number:'))
+b = int(input('enter 2nd number:'))
+print("sum is:",a+b)
