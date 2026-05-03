4598a144b5ff73fdb1404224e75645f7f749d044
Abhishree Raj <abhishree03star@gmail.com>
2026-05-03 18:11:24 +0530

    commit

--- hello.py
+++ hello.py
@@ -1,5 +1,34 @@
 print("Hello, World!")
 print("I am learning AI!!")
-import requests 
+'''import requests 
 response = requests.get("https://api.github.com")
 print(response.status_code) #should return 200 if successful.
+'''
+first_name = 'abhi'
+last_name = "raj"
+
+full_name = first_name+ last_name
+print(full_name)
+
+long_dash = "-" *10
+print(long_dash)
+print(len(long_dash))
+print(len(full_name))
+
+is_raining = True
+print(is_raining) 
+age = 16
+can_vote = age >=18
+print(can_vote) 
+
+age = 25
+has_license = True
+drunk = False
+can_drive = age >= 16 and has_license and drunk !=True
+print(can_drive)
+
+
+
+string = "hi my name is if"
+name = f'if {string}'
+print(name)
